6d0a5372cb00f5456655874b981cd13e8a4285bd
juampi74 <juampijaca@gmail.com>
2024-04-08 21:16:27 -0300

    Aclaré los mensajes mostrados en consola

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ console.log("Salioooo!");
 const deporteNahu = "Tenis";
 
 if (deporteNahu == "Futbol") {
-  console.log("Banqué!");
+  console.log(deporteNahu + ": Banqué!");
 } else if (deporteNahu == "Tenis") {
   console.log(
-    "Tiene que pasar por el comité de chequeo (99 días hábiles de espera)"
+    deporteNahu +
+      ": Tiene que pasar por el comité de chequeo (99 días hábiles de espera)"
   );
 } else {
-  console.log("Se cae");
+  console.log(deporteNahu + ": Se cae");
 }
